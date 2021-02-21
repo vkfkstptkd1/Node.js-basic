eb0fe3397ae85e9e8b6d3bc06fdf600607abc9ec
vkfkstptkd1 <vkfkstptkd1@naver.com>
2021-02-21 21:27:51 +0900

    Node.js-웹서버 만들기- 주석 추가

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ var app = http.createServer(function(request,response){
 	}
 	response.writeHead(200);
 	response.end(fs.readFileSync(__dirname+url));
-
+	//__dirname : 현재 파일이 위치한경로
+	//url : 사용자가 페이지에서 요청한 것에 대한 url ex) /1.html
+	//fs.readFileSync() : 괄호 안에 있는 경로의 파일을 가져와라
 	});
 app.listen(3000);
